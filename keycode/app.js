ec15c46b09a42b18675a9ca012b1fb9f208c55c2
Nicholas Ferrari <Nick.ferrari@me.com>
2021-02-03 14:39:03 +0000

    getting table to hide on page load

--- keycode/app.js
+++ keycode/app.js
@@ -1,14 +1,15 @@
 const showhidebtn = document.getElementById("tableToggle");
 const table = document.getElementById("tab");
 
+//This will hide the table when page first loaded and then add table when button clicked.
 showhidebtn.addEventListener("click", () => {
 
     if(table.style.display == "none") {
         table.style.display = "block";
-        showhidebtn.textContent = "back";
+        showhidebtn.textContent = "Back";
     }
     else {
         table.style.display = "none";
-        showhidebtn.textContent = "table";
+        showhidebtn.textContent = "Table";
     }
 });
