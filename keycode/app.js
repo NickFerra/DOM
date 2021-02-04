5b4a3cbffe20462b88ccd1470dad90fe6abf66fa
Nicholas Ferrari <Nick.ferrari@me.com>
2021-02-04 12:31:43 +0000

    adding keydown event

--- keycode/app.js
+++ keycode/app.js
@@ -1,15 +1,37 @@
-const showhidebtn = document.getElementById("tableToggle");
+const showHideBtn = document.getElementById("tableToggle");
 const table = document.getElementById("tab");
+const textKey = document.getElementById("text-key");
+const textCode = document.getElementById("text-code");
+const textWhich = document.getElementById("text-which");
+const text = document.getElementById("text-display");
+const events1 = document.getElementById("events1");
+const events2 = document.getElementById("events2");
+const events3 = document.getElementById("events3");
 
 //This will hide the table when page first loaded and then add table when button clicked.
-showhidebtn.addEventListener("click", () => {
+showHideBtn.addEventListener("click", () => {
 
     if(table.style.display == "none") {
         table.style.display = "block";
-        showhidebtn.textContent = "Back";
+        showHideBtn.textContent = "Back";
     }
     else {
         table.style.display = "none";
-        showhidebtn.textContent = "Table";
+        showHideBtn.textContent = "Table";
     }
+});
+
+//adding keydown event
+document.addEventListener("keydown", (event) =>{
+    
+        events1.style.display = "block";
+        textKey.textContent = (event.key);
+
+        events2.style.display = "block";
+        textCode.textContent = (event.code); 
+
+        events3.style.display = "block";
+        textWhich.textContent = (event.which);
+    
+   
 });
